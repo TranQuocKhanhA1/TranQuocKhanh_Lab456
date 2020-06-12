43c986e6bd130c736b7d9e35fe6b4f45fa659d33
Administrator <Administrator@PC254>
2020-06-12 15:22:50 +0700

    Views/Course/Create.cshtml

--- tramquockhanh_lab456/ViewModels/CourseViewModel.cs
+++ tramquockhanh_lab456/ViewModels/CourseViewModel.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using tramquockhanh_lab456.Models;
 
 namespace tramquockhanh_lab456.ViewModels
 {
@@ -10,5 +11,11 @@ namespace tramquockhanh_lab456.ViewModels
         public string Place { get; set; }
         public string Date { get; set; }
         public string Time { get; set; }
+
+        public IEnumerable<Category> Categories { get; set; }
+        public DateTime GetDateTime()
+        {
+            return DateTime.Parse(string.Format("{0} {1}", Date, Time));
+        }
     }
 }
