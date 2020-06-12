9cd11f28e5b3588c5dc95162e63a295507e7d42f
Administrator <Administrator@PC254>
2020-06-12 14:38:44 +0700

    Create

---
+++ tramquockhanh_lab456/Controllers/CoursesController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace tramquockhanh_lab456.Controllers
+{
+    public class CoursesController : Controller
+    {
+        // GET: Courses
+        public ActionResult Create()
+        {
+            return View();
+        }
+    }
+}
