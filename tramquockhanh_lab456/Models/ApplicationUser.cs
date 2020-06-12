dce4c381ec42d98dd221b1bd91d72f756a88fe62
Administrator <Administrator@PC254>
2020-06-12 14:25:56 +0700

    add-migration 'CreateTableCourse'

--- tramquockhanh_lab456/Models/ApplicationUser.cs
+++ tramquockhanh_lab456/Models/ApplicationUser.cs
@@ -20,6 +20,8 @@ namespace tramquockhanh_lab456.Models
 
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Category> Categories { get; set; }
         public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
         {
---
+++ tramquockhanh_lab456/Models/Category.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace tramquockhanh_lab456.Models
+{
+    public class Category
+    {
+       
+            public byte Id { get; set; }
+            [Required]
+            [StringLength(255)]
+            public string Name { get; set; }
+        
+    }
+}
