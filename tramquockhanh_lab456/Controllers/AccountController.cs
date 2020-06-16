74445a7103d060fe4faa5728c64715e959384ff4
Khánh Trần <quockhanhitdakia@gmail.com>
2020-06-17 01:38:50 +0700

    trang 34

--- tramquockhanh_lab456/Controllers/AccountController.cs
+++ tramquockhanh_lab456/Controllers/AccountController.cs
@@ -156,7 +156,7 @@ namespace tramquockhanh_lab456.Controllers
         {
             if (ModelState.IsValid)
             {
-                var user = new ApplicationUser { UserName = model.Email, Email = model.Email };
+                var user = new ApplicationUser { UserName = model.Email, Email = model.Email, Name = model.Name };
                 var result = await UserManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
                 {
